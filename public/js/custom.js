78928b4972d20e962779bc899d107567cbbd56f4
Johan van den Dorpe <johan@johan.org.uk>
2014-04-04 12:49:41 +0100

    Add HTML and JS to display alert if any servers fail health check

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,3 +3,30 @@ $(function(){
 $("#sortable").tablesorter();
 });
 });
+
+$('#server-status').hide();
+
+function checkhealth() {
+  $.ajax({
+    type: 'GET',
+      url: '/health',
+      dataType: 'json',
+      success: function(data, textStatus, XMLHttpRequest) {
+        if (data.length > 0) {
+          message = "Health check failed for " + data.join(', ');
+          $('#server-status-message').text(message)
+          $('#server-status').show();
+
+          console.log(data)
+        } else {
+          $('#server-status').hide();
+        }
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        console.log('Failure getting health check');
+      }
+  });
+}
+
+checkhealth();
+window.setInterval(checkhealth, 10000);
